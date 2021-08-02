f09656e8f46c5a9f0ba8617c5d3ec6ca3bfe5235
maedrome <maedrome@espol.edu.ec>
2021-08-01 22:22:41 -0500

    actualizado

---
+++ app/src/main/java/com/example/lab5_grupo05_main/PerfilUsuario.java
@@ -0,0 +1,56 @@
+package com.example.lab5_grupo05_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+public class PerfilUsuario extends AppCompatActivity {
+    TextView txt_id, txt_name, txt_email;
+    ImageView imv_photo;
+    DatabaseReference db_reference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>)intent.getSerializableExtra("info_user");
+
+        txt_id = findViewById(R.id.txt_userId);
+        txt_name = findViewById(R.id.txt_nombre);
+        txt_email = findViewById(R.id.txt_correo);
+        imv_photo = findViewById(R.id.imv_foto);
+
+        txt_id.setText(info_user.get("user_id"));
+        txt_name.setText(info_user.get("user_name"));
+        txt_email.setText(info_user.get("user_email"));
+        String photo = info_user.get("user_photo");
+        Picasso.with(getApplicationContext()).load(photo).into(imv_photo);
+        iniciarBaseDeDatos();
+    }
+    public void cerrarSesion(View view){
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", "cerrarSesion");
+        startActivity(intent);
+    }
+    public void iniciarBaseDeDatos(){
+        db_reference = FirebaseDatabase.getInstance().getReference().child("Grupo");
+        }
+    public void irRegistros(View view){
+        Intent intent = new Intent(this, registros.class);
+        startActivity(intent);
+    }
+}
